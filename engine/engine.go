3630e5af811e8aec8cbbcd1522e8de86c3d95c1b
yuting <515764717@qq.com>
2023-05-04 17:50:07 +0800

    GetSegmenter method

--- engine/engine.go
+++ engine/engine.go
@@ -62,6 +62,15 @@ type Engine struct {
 	persistentStorageInitChannel           chan bool
 }
 
+// GetSegmenter
+//
+//	@Description: 获取分词器
+//	@receiver engine
+//	@return *sego.Segmenter
+func (engine *Engine) GetSegmenter() *sego.Segmenter {
+	return engine.segmenter
+}
+
 func (engine *Engine) Init(options types.EngineInitOptions) {
 	// 将线程数设置为CPU数
 	runtime.GOMAXPROCS(runtime.NumCPU())
@@ -236,14 +245,15 @@ func (engine *Engine) Init(options types.EngineInitOptions) {
 // 将文档加入索引
 //
 // 输入参数：
-//  docId	      标识文档编号，必须唯一，docId == 0 表示非法文档（用于强制刷新索引），[1, +oo) 表示合法文档
-//  data	      见DocumentIndexData注释
-//  forceUpdate 是否强制刷新 cache，如果设为 true，则尽快添加到索引，否则等待 cache 满之后一次全量添加
+//
+//	docId	      标识文档编号，必须唯一，docId == 0 表示非法文档（用于强制刷新索引），[1, +oo) 表示合法文档
+//	data	      见DocumentIndexData注释
+//	forceUpdate 是否强制刷新 cache，如果设为 true，则尽快添加到索引，否则等待 cache 满之后一次全量添加
 //
 // 注意：
-//      1. 这个函数是线程安全的，请尽可能并发调用以提高索引速度
-//      2. 这个函数调用是非同步的，也就是说在函数返回时有可能文档还没有加入索引中，因此
-//         如果立刻调用Search可能无法查询到这个文档。强制刷新索引请调用FlushIndex函数。
+//  1. 这个函数是线程安全的，请尽可能并发调用以提高索引速度
+//  2. 这个函数调用是非同步的，也就是说在函数返回时有可能文档还没有加入索引中，因此
+//     如果立刻调用Search可能无法查询到这个文档。强制刷新索引请调用FlushIndex函数。
 func (engine *Engine) IndexDocument(docId uint64, data types.DocumentIndexData, forceUpdate bool) {
 	engine.internalIndexDocument(docId, data, forceUpdate)
 
@@ -273,13 +283,14 @@ func (engine *Engine) internalIndexDocument(
 // 将文档从索引中删除
 //
 // 输入参数：
-//  docId	      标识文档编号，必须唯一，docId == 0 表示非法文档（用于强制刷新索引），[1, +oo) 表示合法文档
-//  forceUpdate 是否强制刷新 cache，如果设为 true，则尽快删除索引，否则等待 cache 满之后一次全量删除
+//
+//	docId	      标识文档编号，必须唯一，docId == 0 表示非法文档（用于强制刷新索引），[1, +oo) 表示合法文档
+//	forceUpdate 是否强制刷新 cache，如果设为 true，则尽快删除索引，否则等待 cache 满之后一次全量删除
 //
 // 注意：
-//      1. 这个函数是线程安全的，请尽可能并发调用以提高索引速度
-//      2. 这个函数调用是非同步的，也就是说在函数返回时有可能文档还没有加入索引中，因此
-//         如果立刻调用Search可能无法查询到这个文档。强制刷新索引请调用FlushIndex函数。
+//  1. 这个函数是线程安全的，请尽可能并发调用以提高索引速度
+//  2. 这个函数调用是非同步的，也就是说在函数返回时有可能文档还没有加入索引中，因此
+//     如果立刻调用Search可能无法查询到这个文档。强制刷新索引请调用FlushIndex函数。
 func (engine *Engine) RemoveDocument(docId uint64, forceUpdate bool) {
 	if !engine.initialized {
 		log.Fatal("必须先初始化引擎")
